41e90d89d4950b331a784f15d4556b32683b501e
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2026-07-07 10:20:40 +0200

    Harden cache clear request sanitization

--- rt-plugin-report.php
+++ rt-plugin-report.php
@@ -95,8 +95,8 @@ if ( is_admin() && ! class_exists( 'RT_Plugin_Report' ) ) {
 			$wp_latest = $this->check_core_updates();
 
 			// Refresh the cache, but only if nonce is valid and this is a fresh timestamp (not if the page has been refreshed with the timestamp still in the URL).
-			if ( isset( $_GET['clear_cache'] ) && isset( $_GET['_wpnonce'] ) && wp_verify_nonce( sanitize_key( $_GET['_wpnonce'] ), 'plugin_report_clear_cache' ) ) {
-				$new_timestamp  = intval( $_GET['clear_cache'] );
+			if ( isset( $_GET['clear_cache'] ) && isset( $_GET['_wpnonce'] ) && wp_verify_nonce( sanitize_key( wp_unslash( $_GET['_wpnonce'] ) ), 'plugin_report_clear_cache' ) ) {
+				$new_timestamp  = absint( wp_unslash( $_GET['clear_cache'] ) );
 				$last_timestamp = intval( get_site_transient( 'plugin_report_cache_cleared' ) );
 				if ( ! $last_timestamp || $new_timestamp > $last_timestamp ) {
 					$this->clear_cache();
